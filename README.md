e400f501b76b069f6aba9732f0dda75cfc096745
coliu19 <coliu@cisco.com>
2022-12-26 19:52:35 +0800

    split completeness to contract and documentation

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ The API Insights OpenAPI ruleset is a project to programatically check conforman
 
 This module implements a set of lintable REST API best practices, linters can be used validate API Specs in Swagger2 or OAS3 format. The project implements 2 linter rulesets. 
 1. REST Guidelines :  Lintable REST API best practices related to API Design, API implementation, API testing and developer experience. For details refer [rest-guidelines.md](docs/rest-guidelines.md)
-2. Doc Completeness: set of rules provided to ensure the end user document generated from API specs are of good quality. It includes rules and examples are included for every schema, description provided for every attribute, etc. For details refer to [doc-completeness.md](docs/doc-completeness.md)
+2. Contract: set of rules provided to ensure the end user document generated from API specs are of good quality. For details refer to [doc-completeness.md](docs/doc-completeness.md)
+2. Documentation: It includes rules and examples are included for every schema, description provided for every attribute, etc. For details refer to [doc-completeness.md](docs/doc-completeness.md)
 
 These custom rulesets are implemented using [Spectral](https://github.com/stoplightio/spectral) open source linter. 
 
@@ -21,7 +22,8 @@ npm install @cisco-developer/api-insights-openapi-rulesets
 There are two rulesets in this repo, you can choose one of them:
 ```
 api-insights-openapi-ruleset.js
-completeness.js
+contract.js
+documentation.js
 ```
 
 * If you installed spectral cli globally refer to [spectral-cli](https://meta.stoplight.io/docs/spectral/b8391e051b7d8-installation), you can run:
@@ -37,7 +39,8 @@ Also you can refer to [use npm](https://meta.stoplight.io/docs/spectral/7895ff11
 ```
 extends:
   - "@cisco-developer/api-insights-openapi-rulesets/api-insights-openapi-ruleset.js"
-  - "@cisco-developer/api-insights-openapi-rulesets/completeness.js"
+  - "@cisco-developer/api-insights-openapi-rulesets/contract.js"
+  - "@cisco-developer/api-insights-openapi-rulesets/documentation.js"
 ```
 
 ## Contributing
--- contract.js
+++ contract.js
@@ -21,9 +21,7 @@ import {
 } from '@stoplight/spectral-formats';
 import { oas } from '@stoplight/spectral-rulesets';
 import completedSchema from './functions/completedSchema.js';
-import ensureExamples from './functions/ensureExamples.js';
 import ensureField from './functions/ensureField.js';
-import ensureVersion from './functions/ensureVersion.js';
 import includeAll from './functions/includeAll.js';
 import keyMatchAnyPattern from './functions/keyMatchAnyPattern.js';
 export default {
@@ -145,39 +143,6 @@ export default {
         },
       },
     },
-    'description-for-every-attribute': {
-      'description': 'DEA - Descriptions for Every Attribute',
-      'message': 'For every attribute that is present in the OAS document, if a description is proposed as optional to complement that attribute, then yes it must be present; {{error}}',
-      'severity': 'warn',
-      'given': [
-        '$.tags[*]',
-        '$.info',
-        '$.server',
-        '$.externalDocs',
-        '$.paths.*.*.parameters.*',
-        '$.paths.*.*.requestBody',
-        '$.paths.*.*.responses.*',
-        '$.paths.*.*.examples.*',
-        '$.paths.*.*.responses.links.*',
-        '$.*.securitySchemes.*',
-        '$.securityDefinitions.*',
-      ],
-      'then': {
-        'function': ensureField,
-        'functionOptions': {
-          'field': 'description',
-        },
-      },
-    },
-    'oas-version': {
-      'description': 'The document must be specify the OAS version it is supporting.',
-      'message': '{{description}}; {{error}}',
-      'severity': 'error',
-      'given': '$',
-      'then': {
-        'function': ensureVersion,
-      },
-    },
     'oas2-meta-info': {
       'description': 'The following fields must be present (note',
       'message': '{{description}} {{error}}',
@@ -198,22 +163,6 @@ export default {
             'securityDefinitions',
           ],
         },
-      },
-    },
-    'examples-for-every-schema': {
-      'description': 'For every schema provided in the OAS document, at least one example must be present',
-      'message': '{{description}}; {{error}}',
-      'severity': 'warn',
-      'given': [
-        '$.paths.*.*.*.*.content[?(@ && @.schema)]',
-        '$.paths.*.*.*.content[?(@ && @.schema)]',
-        '$.paths.*.*.responses[*].headers[?(@ && @.schema)]',
-        '$.paths.*.*.parameters[?(@ && @.schema)]',
-        '$.paths.*.*.responses[?(@ && @.schema)]',
-        "$.paths.*.*.parameters[?(@ && @.schema && @.in === 'body')]",
-      ],
-      'then': {
-        'function': ensureExamples,
       },
     },
   },
