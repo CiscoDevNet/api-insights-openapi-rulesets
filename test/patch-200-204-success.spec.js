d28c9ab17c714c02d2101732bae4152aa2651d48
Toby Vestal <tvestal@cisco.com>
2023-02-14 12:08:53 -0500

    update patch-200-204 test

--- test/patch-200-204-success.spec.js
+++ test/patch-200-204-success.spec.js
@@ -15,11 +15,11 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import fsPromises from 'fs/promises';
 import path from 'path';
 import { prepLinter } from '../util/testUtils';
 import ruleset from '../api-insights-openapi-ruleset';
+
 const ruleName = 'patch-200-204-success';
 const resPath = path.join(__dirname, `resources/${ ruleName }`);
 
@@ -110,8 +110,22 @@ describe(ruleName, () => {
       },
     ]);
   });
-  test('should catch PATCHs containing info not in response body, indicating possible partial representation response', async () => {
-    const spec = await fsPromises.readFile(`${ resPath }/negative-partial-rep.yml`);
+  test('should pass PATCHs with 200 status code', async () => {
+    const spec = await fsPromises.readFile(`${ resPath }/positive-200.yml`);
+    const res = await spectral.run(spec.toString());
+
+    expect(res).toEqual([]);
+  });
+  test('should pass PATCHs with 204 status code', async () => {
+    const spec = await fsPromises.readFile(`${ resPath }/positive-204.yml`);
+    const res = await spectral.run(spec.toString());
+
+    expect(res).toEqual([]);
+  });
+
+
+  test('should catch PATCHs with both 200 and 204 status codes - oas2', async () => {
+    const spec = await fsPromises.readFile(`${ resPath }/negative-both-oas2.yml`);
     const res = await spectral.run(spec.toString());
 
     expect(res).toEqual([
@@ -125,34 +139,84 @@ describe(ruleName, () => {
         ],
         range: {
           start: {
-            line: 26,
+            line: 25,
             character: 10,
           },
           end: {
-            line: 46,
-            character: 30,
+            line: 41,
+            character: 32,
           },
         },
         severity: 0,
       },
     ]);
   });
-  test('should pass PATCHs with 200 status code', async () => {
-    const spec = await fsPromises.readFile(`${ resPath }/positive-200.yml`);
+  test('should catch PATCHs without a 200 or 204 status code - oas2', async () => {
+    const spec = await fsPromises.readFile(`${ resPath }/negative-neither-oas2.yml`);
+    const res = await spectral.run(spec.toString());
+
+    expect(res).toEqual([
+      {
+        code: ruleName,
+        message: 'PATCH operations return either \'200 OK\' with full representation or \'204 No Content\' (https://developer.cisco.com/docs/api-insights/#!api-guidelines-analyzer)',
+        path: [
+          'paths',
+          '/test',
+          'patch',
+        ],
+        range: {
+          start: {
+            line: 25,
+            character: 10,
+          },
+          end: {
+            line: 35,
+            character: 32,
+          },
+        },
+        severity: 0,
+      },
+    ]);
+  });
+
+  test('should catch PATCHs containing info not in response body, indicating possible partial representation response - oas2', async () => {
+    const spec = await fsPromises.readFile(`${ resPath }/negative-partial-rep-oas2.yml`);
+    const res = await spectral.run(spec.toString());
+
+    expect(res).toEqual([
+      {
+        code: ruleName,
+        message: 'PATCH operations return either \'200 OK\' with full representation or \'204 No Content\' (https://developer.cisco.com/docs/api-insights/#!api-guidelines-analyzer)',
+        path: [
+          'paths',
+          '/test',
+          'patch',
+        ],
+        range: {
+          start: {
+            line: 25,
+            character: 10,
+          },
+          end: {
+            line: 35,
+            character: 25,
+          },
+        },
+        severity: 0,
+      },
+    ]);
+  });
+  test('should pass PATCHs with 200 status code - oas2', async () => {
+    const spec = await fsPromises.readFile(`${ resPath }/positive-200-oas2.yml`);
     const res = await spectral.run(spec.toString());
 
     expect(res).toEqual([]);
   });
-  test('should pass PATCHs with 204 status code', async () => {
-    const spec = await fsPromises.readFile(`${ resPath }/positive-204.yml`);
+  test('should pass PATCHs with 204 status code - oas2', async () => {
+    const spec = await fsPromises.readFile(`${ resPath }/positive-204-oas2.yml`);
     const res = await spectral.run(spec.toString());
 
     expect(res).toEqual([]);
   });
-  test('should pass partial PATCHs with 200 status code', async () => {
-    const spec = await fsPromises.readFile(`${ resPath }/positive-partial-rep.yml`);
-    const res = await spectral.run(spec.toString());
 
-    expect(res).toEqual([]);
-  });
 });
